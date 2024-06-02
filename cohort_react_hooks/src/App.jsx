f6702565c12d119f2c3d1fa26bcfbd7c8408d08f
RIKTESH89 <rikteshsingh28801@gmail.com>
2024-06-02 10:28:32 +0530

    Learned about React hooks like useEffect

--- cohort_react_hooks/src/App.jsx
+++ cohort_react_hooks/src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
+import { useEffect,useState } from 'react'
+import axios from "axios"
 import './App.css'
 
 function App() {
-
+const [id,setId] = useState(1);
   return (
-    <>
-      
-    </>
+    <div>
+      <button onClick={function(){setId(1)}}>1</button>
+      <button onClick={function(){setId(2)}}>2</button>
+      <button onClick={function(){setId(3)}}>3</button>
+      <button onClick={function(){setId(4)}}>4</button>
+      <Todo id={id}></Todo>
+    </div>
   )
 }
+
+function Todo({id}){
+  const [todo,setTodo] = useState({});
+
+  useEffect(function(){
+    axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+    .then(function(res){
+      setTodo(res.data.todo);
+    })
+  },[id]);
+
+  return(
+    <div>
+      <h1>{todo.title}</h1>
+      <h2>{todo.description}</h2>
+    </div>
+  )
+}
+
+
 
 export default App

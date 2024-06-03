75ccd17ef1a620d603257e865c835ccc8d4fe6eb
RIKTESH89 <rikteshsingh28801@gmail.com>
2024-06-03 11:10:29 +0530

    useRecoilStateLoadable and useRecoilValueLoadable

--- recoil_deep_dive/selector-family/src/App.jsx
+++ recoil_deep_dive/selector-family/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useRecoilState, useRecoilStateLoadable } from 'recoil';
 import { todosAtomFamily } from './atoms';
 
 function App() {
@@ -14,17 +14,36 @@ function App() {
 }
 
 function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+   const [todo, setTodo] = useRecoilStateLoadable(todosAtomFamily(id));
    // get a particular id item i.e., each item needs a specific backend call 
    // like we wanted here <Todo id={1}/>
 
+   // similary if you only want value i.e., const todo, then use useRecoilValueLoadable
+  //  now todo contains an object
+  //  {
+  //   contents 
+  //   state   
+  //  }
+
+  if(todo.state=="loading"){
+    return <div>loading...</div>
+  }
+
+  else if(todo.state=="hasValue"){
   return (
     <>
-      {todo.title}
-      {todo.description}
+      {todo.contents.title}
+      {todo.contents.description}
       <br />
     </>
   )
+  }
+  else if(todo.state=="hasError"){
+    return(
+      <div>Error while getting data from backend</div>
+    )
+  }
+
 }
 
 export default App
